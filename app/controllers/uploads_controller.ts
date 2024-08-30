1cd31f87b42fa892572460e1dce684d44056dd0f
arildo <arildohp@gmail.com>
2024-08-30 17:39:52 -0300

    atualização

--- app/controllers/uploads_controller.ts
+++ app/controllers/uploads_controller.ts
@@ -1,5 +1,3 @@
-// import type { HttpContext } from '@adonisjs/core/http'
-
 import { HttpContext } from '@adonisjs/core/http'
 import { uploadValidator } from '#validators/upload'
 import { GoogleGenerativeAI } from '@google/generative-ai'
@@ -29,25 +27,12 @@ async function process_with_gemini(image: string, type: string): Promise<{ value
 export default class UploadsController {
   async upload({ request }: HttpContext) {
 
-    /// Valida
     const data = request.all()
     const validated = await uploadValidator.validate(data)
 
-    // Retorna erro que corresponda o erro de validacao
-    //
-    // Chama o API do google gemini com os parametros dados no request
-
     const response = await process_with_gemini(validated.image, validated.measure_type)
 
-    // salva esse report na database
-    // customer_code + measure_type
-
     const bill = await Bill.query().exec()
-
-    // const bill = await Bill.create({
-    //   username: 'rlanz',
-    //   email: 'romain@adonisjs.com',
-    // })
 
     return {
       code: bill?.$extras.customer_code,
